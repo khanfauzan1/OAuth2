d8187b32befde646eef0b7d52018df13d0d4fd81
FAUZAN AHMAD <110845300+khanfauzan1@users.noreply.github.com>
2024-01-18 00:53:23 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 passport.use(new GoogleStrategy({
     clientID:    process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: "https://oauth2-alkm.onrender.com/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
